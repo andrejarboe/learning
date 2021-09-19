41b733eda674c304f0c21f67c135de4ca2d8f58d
andrejarboe <andrejarboe@gmail.com>
2021-09-19 18:50:51 -0400

    save

--- javascript/captureEmail/js/app.js
+++ javascript/captureEmail/js/app.js
@@ -1,47 +1,83 @@
+window.onload = function () {
+  let emailState = false;
+  let emailModal = document.getElementsByClassName('email-modal')[0];
+  let closeButton = document.getElementsByClassName(
+    'email-modal__close-btn'
+  )[0];
+  let emailInput = document.getElementsByClassName('email-modal__input')[0];
+  let emailButton = document.getElementsByClassName('email-modal__button')[0];
+  let thankContainer = document.getElementsByClassName('email-thank')[0];
+  let declineOffer = document.getElementsByClassName(
+    'email-modal__decline-offer'
+  )[0];
 
-window.onload = function(){
-    let emailState = false;
-    let emailModal = document.getElementsByClassName('email-modal')[0]; 
-    let closeModal = document.getElementsByClassName('email-modal__close-btn')[0];
-    let emailInput = document.getElementsByClassName('email-modal__input')[0];
-    let emailButton = document.getElementsByClassName('email-modal__button')[0];
+  console.log(emailModal);
+  console.log(closeButton);
 
-    console.log(emailModal);
-    console.log(closeModal);
+  function emailIsValid(email) {
+    return /\S+@\S+\.\S+/.test(email);
+  }
 
-    closeModal.addEventListener('click', () => {
-        emailModal.classList.remove('email-modal--visible');
-    });
-
-    function emailIsValid(email){
-        return /\S+@\S+\.\S+/.test(email);
+  let showModal = () => {
+    if (emailState === false) {
+      emailModal.classList.add('email-modal--visible');
+      console.log('mouse left');
+      // emailState = true;
     }
+  };
 
-    emailButton.addEventListener('click', () => {
-        if(emailIsValid(emailInput.value)){
-                console.log(emailInput.value)
-        }else{
-            document.getElementsByClassName('email-modal__form-group')[0]
-            .classList.add('email-modal__form-group--error');
-            document.getElementsByClassName('email-modal__error-message')[0]
-            .classList.add('email-modal__error-message--active');
-        }
-    })
+  let closeModal = () => {
+    emailModal.classList.remove('email-modal--visible');
+  };
 
+  let addErrors = () => {
+    document
+      .getElementsByClassName('email-modal__form-group')[0]
+      .classList.add('email-modal__form-group--error');
+    document
+      .getElementsByClassName('email-modal__error-message')[0]
+      .classList.add('email-modal__error-message--active');
+  };
 
-    let showModal = () => {
-        if(emailState === false){
-            emailModal.classList.add('email-modal--visible');
-            console.log('mouse left');
-            // emailState = true;
-        }
+  let removeErrors = () => {
+    document
+      .getElementsByClassName('email-modal__form-group')[0]
+      .classList.remove('email-modal__form-group--error');
+    document
+      .getElementsByClassName('email-modal__error-message')[0]
+      .classList.remove('email-modal__error-message--active');
+  };
 
+  let showThankMessage = () => {
+    thankContainer.classList.add('email-thank--success');
+    setTimeout(() => {
+      closeModal();
+    }, 3000);
+  };
+
+  closeButton.addEventListener('click', () => {
+    closeModal();
+  });
+
+  emailInput.addEventListener('click', () => {
+    removeErrors();
+  });
+
+  emailButton.addEventListener('click', () => {
+    if (emailIsValid(emailInput.value)) {
+      // console.log(emailInput.value);
+      showThankMessage();
+    } else {
+      addErrors();
     }
-    
-    document.body.addEventListener('mouseleave', 
-        () => {
-            showModal();
-        }
-    );
-    console.log(document);
-}
+  });
+
+  declineOffer.addEventListener('click', () => {
+    closeModal();
+  });
+
+  document.body.addEventListener('mouseleave', () => {
+    showModal();
+  });
+  console.log(document);
+};
