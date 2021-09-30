c2acd63ae32bc8bbc7c8dfd383b7e6f483789cea
andrejarboe <andrejarboe@gmail.com>
2021-09-29 21:05:34 -0400

    add bootstrap and icons

--- java/helloworld/hello.java
+++ java/helloworld/hello.java
@@ -1,5 +1,10 @@
 class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello");
+        System.out.println("Hello 2");
+        
+        int hello = 5;
+        
+        System.out.println(hello);
     }
 }
