d44fb432cc1acf315702fae43a7981da7f9cb787
andrejarboe <andrejarboe@gmail.com>
2021-10-11 13:30:01 -0400

    save

--- nodejs/express-intro/app/routes.js
+++ nodejs/express-intro/app/routes.js
@@ -5,5 +5,17 @@ router.get('/', (req, res) => {
   res.render('../assets/views/homepage.pug');
 });
 
+router.get('/pug', (req, res) => {
+  res.render('../assets/views/testing.pug',
+    {
+      username: "andrejarboe",
+      fname: "Andre",
+      lname: "Jarboe",
+      loggedIn: true,
+      friends: ['Jane', 'Mike', 'Stephanie', "Jessica"]
+    }
+  );
+});
+
 
 module.exports = router;
