ddc876738f7703668b0f0bfa27c68aedfc5dbdba
andrejarboe <andrejarboe@gmail.com>
2021-10-29 19:24:57 -0400

    top nav mvp

--- amazon-clone/src/components/Header.js
+++ amazon-clone/src/components/Header.js
@@ -1,4 +1,9 @@
 import Image from 'next/image';
+import {
+    MenuIcon,
+    SearchIcon,
+    ShoppingCartIcon
+} from "@heroicons/react/outline"
 
 export default function Header() {
   return (
@@ -16,9 +21,28 @@ export default function Header() {
         />
         </div>
         {/* Search */}
-        <div>
-            <input type="text" />
+        <div className="hidden sm:flex item-center h-10 rounded-md flex-grow  cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+            <input className="p-2 h-full w-6 flex-grow flex-shrink rounded-l-md focus:outline-none" type="text" />
+            <SearchIcon className="h-12 p-4" />
         </div>
+        {/* Search End*/}
+        {/* Right */}
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+            <div className="link">
+                <p>Hello Andre Jarboe II</p>
+                <p className="font-extrabold md:text-sm">Account & List</p>
+            </div>
+            <div className="link">
+                <p>Returns</p>
+                <p className="font-extrabold md:text-sm">& orders</p>
+            </div>
+            <div className="relative link flex items-center">
+                <span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">0</span>
+                <ShoppingCartIcon className="h-10"/>
+                <p className="hidden md:inline font-extrabold md:text-sm mt-2">Basket</p>
+            </div>
+        </div>
+        {/* Right End */}
       </div>
       {/* bottom nav  */}
       <div className=""></div>
