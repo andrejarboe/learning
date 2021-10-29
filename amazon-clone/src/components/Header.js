3d3af3ee6e55a799c52e5435c9aa71fb2c11a566
andrejarboe <andrejarboe@gmail.com>
2021-10-29 18:26:30 -0400

    add npm install @heroicons/react

--- amazon-clone/src/components/Header.js
+++ amazon-clone/src/components/Header.js
@@ -4,13 +4,20 @@ export default function Header() {
   return (
     <header>
       {/* top nav  */}
-      <div className="">
-        <div>
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className='mt-2 flex items-center flex-grow sm:flex-grow-0'>
           <Image 
-            src="https://links.papareact.com/f/90" 
+            src="https://links.papareact.com/f90" 
+            // src="/assets/images/perez-logo.png" 
             width={150}
             height={40}
-            />
+            objectFit='contain'
+            className="cursor-pointer"
+        />
+        </div>
+        {/* Search */}
+        <div>
+            <input type="text" />
         </div>
       </div>
       {/* bottom nav  */}
