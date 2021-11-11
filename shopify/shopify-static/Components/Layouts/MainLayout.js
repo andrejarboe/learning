ca74efc2095ce12832ae6f7a2acd09c6da13fa31
Andre Jarboe <andrejarboe@gmail.com>
2021-11-11 14:37:04 -0500

    add hero styles

--- shopify/shopify-static/Components/Layouts/MainLayout.js
+++ shopify/shopify-static/Components/Layouts/MainLayout.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Header1 from "../Header/Header1/Header1";
-import Header2 from '../Header/Header2/Header2';
-import Header3 from '../Header/Header3/Header3';
+// import Header2 from '../Header/Header2/Header2';
+// import Header3 from '../Header/Header3/Header3';
 
 export default function MainLayout(props) {
     return (
@@ -17,8 +17,6 @@ export default function MainLayout(props) {
                 <title>Company | </title>
             </Head>
             <Header1 />
-            <Header2 />
-            <Header3 />
             <main>{props.children}</main>
             <footer>Footer</footer>
         </>
