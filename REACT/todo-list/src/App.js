d7e9ff0cd81068c55c423ed3bdf59438ee486b4d
Andre Jarboe <andrejarboe@gmail.com>
2022-01-09 23:01:23 -0500

    started html strcture

--- REACT/todo-list/src/App.js
+++ REACT/todo-list/src/App.js
@@ -3,8 +3,18 @@ import Header from './components/Header';
 
 export default function App() {
   return (
-    <div className="bg-gray-700 h-screen">
-        <Header />
+    <div className="bg-[#18181f] min-h-screen min-w-screen text-white">
+      <Header />
+      <div className="main flex w-screen">
+        <div className="main-content w-screen flex justify-center ease-in-out duration-[.3s]">
+          <div className='max-w-[700px] w-full'>
+            <div className='my-[50px] text-[30px] font-bold'>Dashboard</div>
+            <div className='mb-[20px] text-[36px] font-extrabold'>Good Morning, Andre</div>
+            <h2>Cooking</h2>
+            <h2>Cooking</h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- REACT/todo-list/src/components/Header.js
+++ REACT/todo-list/src/components/Header.js
@@ -2,23 +2,25 @@ import { FaBars, FaBorderAll, FaImages } from 'react-icons/fa';
 
 export default function Header() {
   return (
-    <div className="container flex">
-      <div className="">
+    <div className="flex h-[70px] items-center bg-[#20212d] px-10 drop-shadow-2xl shadow-2xl w-full space-x-4 sticky top-0">
+      <div className="text-white cursor-pointer">
         <FaBars />
       </div>
-      <div className="">
+      <div className="text-white flex items-center space-x-2 hover:bg-gray-900 px-[16px] py-[10px] rounded cursor-pointer">
         <FaBorderAll />
+        <span>Dashboard</span>
       </div>
-      <div className="">
+      <div className="text-white flex items-center space-x-2 hover:bg-gray-900 px-[16px] py-[10px] rounded cursor-pointer">
         <FaImages />
+        <span>Collections</span>
       </div>
-      <div className="">
-        <div>
-          <img
-            src="https://images.pexels.com/photos/7571856/pexels-photo-7571856.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-            alt="Man in Black Crew Neck Shirt"
-          />
-        </div>
+      <div className="flex-1"></div>
+      <div className="text-white flex items-center">
+        <img
+          src="https://images.pexels.com/photos/7571856/pexels-photo-7571856.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+          alt="Man in Black Crew Neck Shirt"
+          className="h-10 rounded-full"
+        />
       </div>
     </div>
   );
